a6aeb89cfd20d41e04a54acae3644636324d5bb9
frguerra <frguerra@uveg.edu.mx>
2023-07-06 23:49:46 -0500

    Agregar archivos de proyecto.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        public string? Name { get; set; }   
+
+        public string Iscomplete { get; set; }
+    }
+}
